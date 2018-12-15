ece5c6f86e3aca0f3b28d9021f0ccba1616935e4
oleksandr.baibarak@nure.ua <oleksandr.baibarak@nure.ua>
2018-12-15 13:14:19 +0200

    Deploy for git

---
+++ usermanagement/src/main/java/ua/nure/kn/baibarak/usermanagement/db/ConnectionFactoryImpl.java
@@ -0,0 +1,47 @@
+package ua.nure.kn.baibarak.usermanagement.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionFactoryImpl implements ConnectionFactory {
+	private String driver = "org.hsqldb.jdbcDriver";
+	private String url = "jdbc:hsqldb:file:db/usermanagement";
+	private String user = "sa";
+	private String password = "";
+	
+	public ConnectionFactoryImpl(String driver, String url, String user, String password) {
+		this.driver = driver;
+		this.url = url;
+		this.user = user;
+		this.password = password;
+	}
+
+	public ConnectionFactoryImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConnectionFactoryImpl(Properties properties) {
+		user = properties.getProperty("connection.user");
+        password = properties.getProperty("connection.password");
+        url = properties.getProperty("connection.url");
+        driver = properties.getProperty("connection.driver");
+	}
+
+	@Override
+	public Connection createConnection() throws DatabaseException {		
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			throw new DatabaseException(e);
+		}
+		
+		try {
+			return DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
+	}
+
+}
